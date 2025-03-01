2d5f7ac4a4a0f8514343ba7a48c6f6427767f6f9
John Benedict Signo <jbsigno@gmail.com>
2025-03-02 00:54:21 +0800

    FINAL

--- IBM FS/HTML-CSS-JS/script.js
+++ IBM FS/HTML-CSS-JS/script.js
@@ -1,28 +1,42 @@
 function addRec(){
-    const inputMes = document.getElementById("mes-r");
-    const inputName = document.getElementById("name-r");
+    const inputMes = document.getElementById("mes-r").value.trim();
+    const inputName = document.getElementById("name-r").value.trim();
     const contBox = document.getElementById("cont-box");
+
+    console.log(`${inputMes} ${inputName}`);
+    
 
     if(inputMes.value === ''){
         alert("You cannot leave your message empty!");
     }
     else{
-        let pn = document.createElement("p");
-        let pm = document.createElement("p");
-        let div = document.createElement("div");
+        let div = document.createElement("li");
         let q = document.createElement("p");
-        q.value = '"'
-
+        let pm = document.createElement("p");
         
         div.classList.add("box");
+        q.classList.add("q");
+        pm.classList.add("rec");
+        
+        
+        q.textContent = '"';
+        pm.textContent = `${inputMes}`;
+        
+
+        div.appendChild(q);
+        div.appendChild(pm);
+
+        if(inputName.value === ''){
+            let pn = document.createElement("p");
+            pn.classList.add("auth-r");
+            pn.textContent = `${inputName}`;
+            div.appendChild(pn);
+        }
+        
         contBox.appendChild(div);
 
-        pm.classList.add("rec");
-
-        pm.innerHTML = inputMes.value;
-        pn.innerHTML = inputName.value;
-        div.appendChild(pm);
-        div.appendChild(pn);
+        document.getElementById("name-r").value = "";
+        document.getElementById("mes-r").value = "";
     }
 
 }
